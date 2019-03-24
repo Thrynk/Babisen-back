408bda753200ceadc32a5a2bdf6aa0c22cae4a64
Thrynk <thomas.carin@isen.yncrea.fr>
2019-03-24 23:46:58 +0100

    Change message response for test

--- routes/webhook/handleMessage.js
+++ routes/webhook/handleMessage.js
@@ -9,7 +9,7 @@ module.exports = function handleMessage(sender_psid, received_message) {
 
     // Create the payload for a basic text message
     response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      "text": `You sent the message: "${received_message.text}"`
     }
   }
 
